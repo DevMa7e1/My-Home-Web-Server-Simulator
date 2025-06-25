19b446bbcaad4a8a4a7eff83d823b97550b5a378
DevMa7e1 <thedevma7e1@gmail.com>
2025-06-25 18:49:20 +0300

    Added upgrades
    In this commit I have added upgrades that increase the visitors per second and the overload warning.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,13 +13,26 @@ public class GameManager : MonoBehaviour
     public double maxVisitorsPerSecond = 0.0;
 
     public double currentRevenue = 0.0;
-    public double adRevenuePerVisitor = 0.01;
+    public double adRevenuePerVisitor = 0.05;
     
     private string[] names = {"ESP-01", "Raspberry Pi Pico W", "ESP-32",
         "Raspberry Pi Zero W", "Raspberry Pi 3", "Raspberry Pi 4",
         "Raspberry Pi 5", "Dell Optiplex 7050"};
     private int[] maxes = { 5, 15, 20, 50, 100, 200, 450, 1000 };
     private int[] prices = { 15, 35, 40, 70, 170, 300, 500, 900 };
+
+    private string[] upgrades = { "Write new content!",
+                                  "Get an HTTPS certificate!",
+                                  "Make posts on social media!",
+                                  "Start posting tutorials!"};
+    private string[] upgMessg = { "New content will bring new visitors.",
+                                  "It is free, greatly improves security and helps a lot with SEO.",
+                                  "With social media, you can attract many new people to your website without needing investing in ads.", 
+                                  "Most of the time, tutorials can stay relevant for longer periods of time compared to articles."};
+    private int[] upgPrices = { 5, 0, 10, 5};
+    private int[] upgGains = { 10, 20, 15, 10};
+
+    public int currentUpgIndx = 0;
 
     public List<int> DeviceIndexes = new();
 
@@ -37,6 +50,14 @@ public class GameManager : MonoBehaviour
 
     public List<Button> buttons = new List<Button>();
 
+    public TMP_Text UpgradeText;
+    public Button UpgradeButton;
+    public TMP_Text OverloadText;
+
+    public GameObject MsgPanel;
+    public Button AnOkButton;
+    public TMP_Text MsgText;
+
     void displayButtons(double money, List<Button> buttons)
     {
         foreach (Button button in buttons)
@@ -53,7 +74,10 @@ public class GameManager : MonoBehaviour
             }
         }
     }
-
+    public void UpdateUpgrade()
+    {
+        UpgradeText.text = upgrades[currentUpgIndx] + "\nCost: " + upgPrices[currentUpgIndx].ToString() + "\nGain: +" + upgGains[currentUpgIndx] + " visitors per second";
+    }
     double CalcMaxVPS(List<int> devices, int[] maxes)
     {
         double max = 0;
@@ -93,11 +117,50 @@ public class GameManager : MonoBehaviour
     {
         DevicesText.text = "Devices: " + GetHumanReadableDeivceNames(DeviceIndexes, names);
         maxVisitorsPerSecond = CalcMaxVPS(DeviceIndexes, maxes);
+        CheckOverload();
     }
+
+    void CheckOverload()
+    {
+        if(maxVisitorsPerSecond < visitorsPerSecond)
+        {
+            OverloadText.text = "Overload! Buy more devices!";
+        }
+        else
+        {
+            OverloadText.text = "";
+        }
+    }
+    void UpgradeButtonTask()
+    {
+        if (upgPrices[currentUpgIndx] <= currentRevenue)
+        {
+            visitorsPerSecond += upgGains[currentUpgIndx];
+            showMessageBox(upgMessg[currentUpgIndx]);
+            currentRevenue -= upgPrices[currentUpgIndx];
+            CheckOverload();
+            currentUpgIndx++;
+            UpdateUpgrade();
+        }
+    }
+
+    void showMessageBox(string text)
+    {
+        MsgPanel.SetActive(true);
+        MsgText.text = text;
+    }
+    void hideMessageBox()
+    {
+        MsgPanel.gameObject.SetActive(false);
+    }
+
     void Start()
     {
-
+        UpgradeButton.onClick.AddListener(UpgradeButtonTask);
+        AnOkButton.onClick.AddListener(hideMessageBox);
+        hideMessageBox();
         UpdateDevices();
+        UpdateUpgrade();
         buttons.Add(b0);
         buttons.Add(b1);
         buttons.Add(b2);
@@ -108,9 +171,8 @@ public class GameManager : MonoBehaviour
         buttons.Add(b7);
         //--Example values--
         //--Remove later!!--
-        visitorsPerSecond = 20f;
+        visitorsPerSecond = 5f;
         DeviceIndexes.Add(0);
-        DeviceIndexes.Add(1);
         UpdateDevices();
     }
 
@@ -118,7 +180,10 @@ public class GameManager : MonoBehaviour
     {
         double effectiveVisitors = System.Math.Min(visitorsPerSecond, maxVisitorsPerSecond);
         currentRevenue += effectiveVisitors * adRevenuePerVisitor * Time.deltaTime;
-        MoneyText.text = "Money: " + currentRevenue.ToString("F2");
+        if(visitorsPerSecond <= 20)
+            MoneyText.text = "Money: " + currentRevenue.ToString("F2");
+        else
+            MoneyText.text = "Money: " + currentRevenue.ToString("F0");
         displayButtons(currentRevenue, buttons);
     }
 }
